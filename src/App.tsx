a9fdb11895004e1a19182fce7fb7c38a65f71e57
Antonia <antoniatch@zohomail.eu>
2023-05-22 15:55:39 +0200

    set up graphql and first query

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,32 @@
 import "./App.css";
+import { useQuery, gql } from "@apollo/client";
+
+const GET_CONTINENTS = gql`
+  query continents {
+    continents {
+      code
+      name
+      countries {
+        code
+        name
+      }
+    }
+  }
+`;
 
 function App() {
-  return <div className="App"></div>;
+  const { loading, error, data } = useQuery(GET_CONTINENTS);
+
+  if (loading) return <p> Loading ...</p>;
+  if (error) return <p> Error: {error.message} </p>;
+
+  return (
+    <div className="App">
+      {data.continents.map((continent: any) => {
+        return <p>{continent.name}</p>;
+      })}
+    </div>
+  );
 }
 
 export default App;
